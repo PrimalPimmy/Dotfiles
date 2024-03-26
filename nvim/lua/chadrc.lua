ec6d4f83911ac4c2c5808d7363c8c39fd9fa61a8
PrimalPimmy <Prashant20.pm@gmail.com>
2024-03-27 02:45:46 +0530

    New neovim config
    Signed-off-by: PrimalPimmy <Prashant20.pm@gmail.com>

---
+++ nvim/lua/chadrc.lua
@@ -0,0 +1,15 @@
+---@type ChadrcConfig
+local M = {}
+
+M.ui = {
+	theme = "gruvbox",
+
+	-- hl_override = {
+	-- 	Comment = { italic = true },
+	-- 	["@comment"] = { italic = true },
+	-- },
+}
+
+M.plugins = "custom.plugins"
+
+return M
